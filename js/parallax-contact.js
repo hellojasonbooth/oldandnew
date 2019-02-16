82fecadd9a6877f2e69a8935a3e48d22f6a9281a
Jason Booth <hello@studiojasonbooth.com>
2019-02-16 14:46:05 +0100

    added contact page and js file for the parallax

--- js/parallax-contact.js
+++ js/parallax-contact.js
@@ -1,24 +1,22 @@
 
-
-const sectionProfile = document.querySelector("section.profile")
-
-// we want to move certain tags, based on how far they are from an anchor point
-// the anchor point will be the middle of a section
+const sectionContact = document.querySelector("section.contact-title")
 
 document.addEventListener('scroll', function () {
+
     const topViewport = window.pageYOffset
     const midViewport = topViewport + (window.innerHeight / 2)
 
-    const topSection = sectionProfile.offsetTop 
-    const midSection = topSection + (sectionProfile.offsetHeight / 2)
+    const topSection = sectionContact.offsetTop 
+    const midSection = topSection + (sectionContact.offsetHeight / 2)
 
     const distanceToSection = midViewport - midSection
 
-    const parallaxTags = sectionProfile.querySelectorAll(`[data-parallax]`)
+    const parallaxTags = sectionContact.querySelectorAll(`[data-parallax]`)
 
     // loop over each parallaxed tag
     parallaxTags.forEach(tag => {
         const speed = parseFloat(tag.getAttribute('data-parallax'))
         tag.style.transform = `translate(0, ${distanceToSection * speed - 50}%) rotate(6deg)`
     })
+
 })
