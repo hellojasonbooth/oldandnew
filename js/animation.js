2561b6309f0bb4ca017830690ee7501c12fda399
Jason Booth <hello@studiojasonbooth.com>
2019-02-16 15:12:52 +0100

    added collections page

--- js/animation.js
+++ js/animation.js
@@ -27,6 +27,44 @@ animatedTags.forEach(tag =>{
   fadeInTags()
 
 
+// this animates links in the footer
+const animatedContactTags = document.querySelectorAll('div.contact-page--links span a')
+
+animatedContactTags.forEach(tag =>{
+    tag.style.opacity = 0
+  })
+
+  const fadeInContactTags = function() {
+  
+    let delay = 0.1
+    
+    animatedContactTags.forEach(tag =>{
+      const tagTop = tag.getBoundingClientRect().top
+      const tagBottom = tag.getBoundingClientRect().bottom
+         
+      if(tagTop < window.innerHeight && tagBottom > 0) {
+        tag.style.animation = `fadeup 0.8s ${delay}s both`
+        delay = delay + 0.2
+      } else {
+        tag.style.opacity = 0
+        tag.style.animation = ""
+      }
+    })
+  }
+
+  fadeInContactTags()
+
+
+
+
+
+
+
+
+
+
+
+
 
   // this animates the mask for the info on the profile page
 
@@ -58,4 +96,5 @@ infoMaskAnimate()
   document.addEventListener('scroll', function () {
     fadeInTags() 
     infoMaskAnimate()
+    fadeInContactTags()
   })
