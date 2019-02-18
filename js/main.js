2e60be9b01ee9b0ca53f9fbd4ac754fc0ebc7907
Jason Booth <hello@studiojasonbooth.com>
2019-02-18 20:55:31 +0100

    added scrollify to project

--- js/main.js
+++ js/main.js
@@ -13,3 +13,9 @@
 // document.addEventListener("DOMContentLoaded", function(event) {
 //       headerTag.classList.add('animated')
 //   })
+
+// $(".pagination a").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $(".panel3").offset().top
+//     }, 1200);
+// });
---
+++ js/scroll.js
@@ -0,0 +1,28 @@
+
+$.scrollify({
+    section : ".collection",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: false,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {
+      
+    }
+  });
+
+
+  $(function() {
+    $.scrollify({
+      section : ".collection",
+    });
+});
