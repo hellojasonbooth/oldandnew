62bebdb00f3ededcb4297708e93f920c1eef7809
Jason Booth <hello@studiojasonbooth.com>
2019-02-24 18:51:18 +0100

    added cursor animation on hover

--- js/cursor.js
+++ js/cursor.js
@@ -6,22 +6,19 @@
 const cursorTag = document.querySelector('div.cursor')
 const linkTag = document.querySelector('div.collection a.collection-link')
 
+const cursorTextTags = document.querySelectorAll('div.cursor span p')
+
 
 // when you mouseover make the cursor bigger
 const growCursor = function () {
+
   cursorTag.classList.add('is-active')
   
-//    if (textTag.classList.contains('open')){
-//        cursorTag.innerHTML = "View"
-//    } else {
-//        cursorTag.innerHTML = "Close"
-//    }
 }
 
 // when you mouseout - make cursor smaller
 const shrinkCursor = function () {
   cursorTag.classList.remove('is-active')
-//   cursorTag.innerHTML = ""
 }
 
 // move cursor 
@@ -33,36 +30,44 @@ const moveCursor = function (x, y) {
 // call function to grow cursor
 linkTag.addEventListener('mouseover', function () {
   growCursor()
-  
-//   if (textTag.classList.contains('open')){
-//     cursorTag.innerHTML = "Close"
-//   } else {
-// 		cursorTag.innerHTML = "View"
-//   }
-//   textTag.style.cursor = 'none'
 })
 
-// call function to grow cursor
-// textTag.addEventListener('click', function () {
-  
-//   if (textTag.classList.contains('open')){
-//     cursorTag.style.backgroundColor = 'blue'
-//     cursorTag.innerHTML = "Close"
-//   } else {
-//     cursorTag.style.backgroundColor = 'black'
-// 		cursorTag.innerHTML = "View"
-//   }
-  
-// })
+
 
 // call function to shrink cursor
 linkTag.addEventListener('mouseout', function () {
   shrinkCursor()
-//   cursorTag.innerHTML = ""
 })
 
 
 // call function to move cursor div based on x and y values
 document.addEventListener('mousemove', function (event) {
   moveCursor(event.pageX, event.pageY)
+})
+
+
+
+
+const cursorTagAnimate = function() {
+
+  let delay = 0.1
+    
+  cursorTextTags.forEach(tag =>{
+        tag.style.animation = `cursorText 0.5s ease ${delay}s both`
+        delay = delay + 0.1
+    })
+
+}
+
+
+linkTag.addEventListener('mouseover', function () {
+  cursorTagAnimate()
+})
+
+linkTag.addEventListener('mouseout', function () {
+  
+  cursorTextTags.forEach(tag =>{       
+    tag.style.animation = ""
+  })
+
 })
