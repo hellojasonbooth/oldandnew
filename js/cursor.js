8dd9a13a6d764518b88efc75c10fe83bad7e98b3
Jason Booth <hello@studiojasonbooth.com>
2019-02-28 20:25:01 +0100

    added cursor hover state on homepage

--- js/cursor.js
+++ js/cursor.js
@@ -2,24 +2,10 @@
 
 
 // div element follows the cursor
-
 const cursorTag = document.querySelector('div.cursor')
-const linkTag = document.querySelector('div.collection a.collection-link')
+const linkAreas = document.querySelectorAll('section div.collection a.collection-link')
+const cursorTextTags = document.querySelectorAll('div.cursor p span')
 
-const cursorTextTags = document.querySelectorAll('div.cursor span p')
-
-
-// when you mouseover make the cursor bigger
-const growCursor = function () {
-
-  cursorTag.classList.add('is-active')
-  
-}
-
-// when you mouseout - make cursor smaller
-const shrinkCursor = function () {
-  cursorTag.classList.remove('is-active')
-}
 
 // move cursor 
 const moveCursor = function (x, y) {
@@ -27,47 +13,44 @@ const moveCursor = function (x, y) {
   cursorTag.style.top = y + 'px'
 }
 
-// call function to grow cursor
-linkTag.addEventListener('mouseover', function () {
-  growCursor()
-})
-
-
-
-// call function to shrink cursor
-linkTag.addEventListener('mouseout', function () {
-  shrinkCursor()
-})
-
-
 // call function to move cursor div based on x and y values
 document.addEventListener('mousemove', function (event) {
   moveCursor(event.pageX, event.pageY)
 })
 
 
-
-
-const cursorTagAnimate = function() {
+// pick all the linkable areas to animate cursor
+linkAreas.forEach(linkArea => {
 
   let delay = 0.1
-    
-  cursorTextTags.forEach(tag =>{
-        tag.style.animation = `cursorText 0.5s ease ${delay}s both`
-        delay = delay + 0.1
+
+  linkArea.addEventListener('mouseover', function(){
+    growCursor()
+
+    cursorTextTags.forEach(tag =>{
+
+      tag.style.animation = `cursorText 0.6s cubic-bezier(.72,0,.01,1) ${delay}s both`
+      delay = delay + 0.1
     })
-
-}
-
-
-linkTag.addEventListener('mouseover', function () {
-  cursorTagAnimate()
-})
-
-linkTag.addEventListener('mouseout', function () {
-  
-  cursorTextTags.forEach(tag =>{       
-    tag.style.animation = ""
   })
 
+  linkArea.addEventListener('mouseout', function(){
+    shrinkCursor()
+
+    cursorTextTags.forEach(tag =>{
+      tag.style.animation = ""
+      delay = 0.1
+    })
+  })
 })
+
+
+// when you mouseover make the cursor bigger
+const growCursor = function () {
+  cursorTag.classList.add('is-active')
+}
+
+// when you mouseout - make cursor smaller
+const shrinkCursor = function () {
+  cursorTag.classList.remove('is-active')
+}
