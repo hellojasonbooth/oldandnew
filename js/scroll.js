85558c0fa2b5e397b9d3c87058743f5830a366f3
Jason Booth <hello@studiojasonbooth.com>
2019-03-03 14:24:16 +0100

    added menu styling and footer active state

--- js/scroll.js
+++ js/scroll.js
@@ -17,3 +17,31 @@ $.scrollify({
     afterResize:function() {},
     afterRender:function() {}
   });
+
+
+const footerHome = document.querySelector('footer.home-footer')
+const instaHome = document.querySelector('#insta')
+const logoHome = document.querySelector('.logohome')
+
+const addClass = function() {
+  
+    const tagTop = footerHome.getBoundingClientRect().top
+    const tagBottom = footerHome.getBoundingClientRect().bottom
+       
+    if(tagTop < window.innerHeight && tagBottom > 0) {
+      footerHome.classList.add('active')
+      logoHome.classList.add('white')
+      instaHome.classList.add('white')
+    } else {
+      footerHome.classList.remove('active')
+      logoHome.classList.remove('white')
+      instaHome.classList.remove('white')
+    }
+
+  }
+  addClass()
+
+
+  document.addEventListener('scroll', function () {
+    addClass()
+  })
