9af8fa1ef6e1d893b43be904856199451c1a04be
Jason Booth <hello@studiojasonbooth.com>
2019-03-10 12:30:22 +0100

    split scss into separate section and added footer logo move

--- js/logomove.js
+++ js/logomove.js
@@ -1,73 +1,67 @@
 
 
-// logo follows cursor in footer
-const sublogoTags = document.querySelectorAll('div.sublogo div')
+
 
 const footerAreaTag = document.querySelector('footer.contactpage-footer')
 
+const logoTags = footerAreaTag.querySelectorAll('footer div.logo-wrap div')
 
-let w = footerAreaTag.clientWidth
-let h = footerAreaTag.clientHeight
+let cursorX = footerAreaTag.offsetLeft + (footerAreaTag.clientWidth / 2)
+let cursorY = footerAreaTag.offsetTop + (footerAreaTag.clientHeight / 2)
+
+let logoX = footerAreaTag.offsetLeft + (footerAreaTag.clientWidth / 2)
+let logoY = footerAreaTag.offsetTop + (footerAreaTag.clientHeight / 2)
 
 
-sublogoTags.forEach(tag => {
-  let mouseX = w / 2
-  let mouseY = h / 2
+footerAreaTag.addEventListener("mousemove", function (event) {
+  cursorX = event.clientX
+  cursorY = event.clientY
+})
   
-  let logoX = w / 2
-  let logoY = h / 2
+footerAreaTag.addEventListener("mouseleave", function () {
+  cursorX = footerAreaTag.offsetLeft + (footerAreaTag.clientWidth / 2)
+  cursorY = footerAreaTag.offsetTop + (footerAreaTag.clientHeight / 2)
+})
+  
+const handleResizeFooter = function () {
+
+
+  logoX = footerAreaTag.offsetLeft + (footerAreaTag.clientWidth / 2)
+  logoY = footerAreaTag.offsetTop + (footerAreaTag.clientHeight / 2)
+
+  cursorX = footerAreaTag.offsetLeft + (footerAreaTag.clientWidth / 2)
+  cursorY = footerAreaTag.offsetTop + (footerAreaTag.clientHeight / 2)
+}
+
+  
+ window.addEventListener('resize', handleResizeFooter)
+  
+  
+ logoTags.forEach(tag => {
+  // code related for each ball here
 
   let speed = parseFloat(tag.getAttribute("data-speed"))
 
   function animate() {
-    let distX = mouseX - logoX
-    let distY = mouseY - logoY
+    let distX = cursorX - logoX
+    let distY = cursorY - logoY
 
     logoX = logoX + (distX * speed)
     logoY = logoY + (distY * speed)
 
-
-    tag.style.left = logoX + 'px'
+    tag.style.left = (logoX - footerAreaTag.offsetLeft) + 'px'
     tag.style.top = logoY + 'px'
 
     requestAnimationFrame(animate)
   }
 
   animate()
-  
-    
-  footerAreaTag.addEventListener("mousemove", function () { 
-   	mouseX = event.pageX
-    mouseY = event.pageY
-  })
-
-  document.addEventListener("scroll", function () { 
-    mouseX = event.pageX
-    mouseY = event.pageY
-})
-  
-  
-  const handleResize = function () {
-    
-    w = footerAreaTag.clientWidth
-	h = footerAreaTag.clientHeight
-    
-    logoX = w / 2
-    logoY = h / 2
-    
-    mouseX = w / 2
-    mouseY = h / 2
-}
-  
-  
-footerAreaTag.addEventListener("mouseleave", function () {
-   	mouseX = w / 2
-  	mouseY = h / 2
   })
   
- 
- window.addEventListener('resize', handleResize)
- 
 
-})
+
+
+
+
+
 
