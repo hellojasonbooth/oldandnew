6c85abd10c1d9ee990096911e661186156685d87
Jason Booth <hello@studiojasonbooth.com>
2019-03-13 20:39:56 +0100

    added collection title animation

---
+++ js/collectiontitle.js
@@ -0,0 +1,20 @@
+
+
+const collectionTitleTags = document.querySelectorAll('section.collection-title div.parallax-title p span')
+
+collectionTitleTags.forEach(tag =>{
+    tag.style.opacity = 0
+})
+
+const collectionTitleAnimate = function() {
+  
+    let delay = 0.1
+    
+    collectionTitleTags.forEach(tag =>{
+        tag.style.animation = `jump 0.5s ${delay}s both`
+        delay = delay + 0.3
+      })
+  }
+
+
+  collectionTitleAnimate()
